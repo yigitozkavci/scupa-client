f3658cb605a0d54cbacf7813b7882a20be466842
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2016-11-24 00:18:03 +0300

    Setup concurrent build system for styling with scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import './index.css';
 import reducers from './reducers';
 import Login from './containers/Login';
 
